1327af9bcc788a1cad4c41f5a45fd74aaea4a958
king-sunshine-spring <1348066599@qq.com>
2017-04-10 16:05:03 +0800

    @wgcwgc  JiaZhangBang
    @wgcwgc  JiaZhangBang
    add TextSize & color change
    @ author  wgcwgc
    @ time 2017.04.10
    @ address runcom
    @ config intel(R) Core (TM) i5 - 2310 cpu @ 2.90GHz 3.20GHz
    8.00GB(7.89GB可用) 64bit
    windows 7 旗舰版
    @ detail mark

--- src/com/runcom/jiazhangbang/reciteText/ReciteTextMain.java
+++ src/com/runcom/jiazhangbang/reciteText/ReciteTextMain.java
@@ -3,8 +3,11 @@
  */
 package com.runcom.jiazhangbang.reciteText;
 
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -17,8 +20,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.runcom.jiazhangbang.R;
@@ -42,7 +47,8 @@ import com.umeng.analytics.MobclickAgent;
 
 public class ReciteTextMain extends Activity
 {
-	EditText editText;
+	TextView contents_textView , score_textView , historyScore_textView;
+	EditText contents_editText;
 	Intent intent;
 
 	/*
@@ -78,7 +84,7 @@ public class ReciteTextMain extends Activity
 
 	private void initView()
 	{
-		editText = (EditText) findViewById(R.id.recite_text_main_text);
+		contents_textView = (TextView) findViewById(R.id.recite_text_main_text);
 		String tempString = "start...\n";
 		int i;
 		for(i = 0 ; i < 7 ; i ++ )
@@ -87,27 +93,83 @@ public class ReciteTextMain extends Activity
 		}
 		tempString += "end...\n";
 
-		editText.setText(tempString);
-		editText.setOnTouchListener(new OnTouchListener()
+		contents_editText = (EditText) findViewById(R.id.recite_text_main_edit_text_score);
+
+		score_textView = (TextView) findViewById(R.id.recite_text_main_textview_userjudge);
+		score_textView.setOnClickListener(new OnClickListener()
 		{
+
+			@Override
+			public void onClick(View v )
+			{
+				// TODO Auto-generated method stub
+				String contents_editText_score = contents_editText.getText().toString();
+				Toast.makeText(getApplicationContext() ,"您提交了 " + contents_editText_score + " 分" ,Toast.LENGTH_SHORT).show();
+
+			}
+		});
+
+		historyScore_textView = (TextView) findViewById(R.id.recite_text_main_textview_history_score);
+		historyScore_textView.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v )
+			{
+				// TODO Auto-generated method stub
+				AlertDialog.Builder builder = new AlertDialog.Builder(ReciteTextMain.this , R.style.NoBackGroundDialog);
+				builder.setIcon(R.drawable.ic_launcher);
+				getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+				builder.setTitle("您的历史成绩");
+				final String [] scores ={ "第01次成绩:          57", "第02次成绩:          77", "第03次成绩:          87", "第04次成绩:          97",
+						"第05次成绩:         100", "第06次成绩:          57", "第07次成绩:          77", "第08次成绩:          87",
+						"第09次成绩:          97", "第10次成绩:         100", "第11次成绩:          57", "第12次成绩:          77",
+						"第13次成绩:          87", "第14次成绩:          97", "第15次成绩:         100", "第16次成绩:          57",
+						"第17次成绩:          77", "第18次成绩:          87", "第19次成绩:          97", "第20次成绩:         100" };
+//				for(int j = 0 ; j < 27 ; j ++ )
+//				{
+//					scores[j] = ( "第" + j + "次成绩:         " + (57 + j) ).toString();
+//					if(j < 10)
+//					{
+//						scores[j] = ( "第0" + j + "次成绩:         " + (57 + j) ).toString();
+//					}
+//				}
+				builder.setItems(scores ,new DialogInterface.OnClickListener()
+				{
+					@Override
+					public void onClick(DialogInterface dialog , int which )
+					{
+						Toast.makeText(getApplication() , scores[which] ,Toast.LENGTH_SHORT).show();
+					}
+				});
+				builder.show();
+			}
+		});
+
+		contents_textView.setText(tempString);
+		contents_textView.setOnTouchListener(new OnTouchListener()
+		{
+			@SuppressLint("ClickableViewAccessibility")
 			@Override
 			public boolean onTouch(View v , MotionEvent event )
 			{
-				touch_count ++ ;
-				switch(touch_count)
-				{
-					case 1:
-						Toast.makeText(getApplicationContext() ,"touch" ,Toast.LENGTH_SHORT).show();
-
-						break;
-					default:
-						Toast.makeText(getApplicationContext() ,"touch: " + touch_count ,Toast.LENGTH_SHORT).show();
-
-				}
+				// touch_count ++ ;
+				// switch(touch_count)
+				// {
+				// case 1:
+				// Toast.makeText(getApplicationContext() ,"touch"
+				// ,Toast.LENGTH_SHORT).show();
+				//
+				// break;
+				// default:
+				// Toast.makeText(getApplicationContext() ,"touch: " +
+				// touch_count ,Toast.LENGTH_SHORT).show();
+				//
+				// }
 				return false;
 			}
 		});
-		editText.addTextChangedListener(new TextWatcher()
+		contents_textView.addTextChangedListener(new TextWatcher()
 		{
 
 			@Override
@@ -132,12 +194,6 @@ public class ReciteTextMain extends Activity
 		});
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.app.Activity#onCreateContextMenu(android.view.ContextMenu,
-	 * android.view.View, android.view.ContextMenu.ContextMenuInfo)
-	 */
 	@Override
 	public void onCreateContextMenu(ContextMenu menu , View view , ContextMenuInfo menuInfo )
 	{
